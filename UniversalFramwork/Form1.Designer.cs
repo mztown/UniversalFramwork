384a326b78ad21284b022a7a4dffe4459bdce66e
叶润桐 <runtongye@live.com>
2017-07-20 15:21:57 +0800

    basic function

--- UniversalFramwork/Form1.Designer.cs
+++ UniversalFramwork/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "基本功能";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(152, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "第二功能";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(241, 74);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "UniversalFramwork";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- UniversalFramwork/Form1.cs
+++ UniversalFramwork/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,9 +13,181 @@ namespace UniversalFramwork
 {
     public partial class Form1 : Form
     {
+        const int initLabelX = 12;
+        const int initLabelY = 17;
+        const int incrementX = 30;
+        const int incrementY = 80;
+        const int initButtonX = 71;
+        const int initButtonY = 12;
+        const int initWindowX = 175;
+        const int initWindowY = 83;
+        const int Version = 1;
+
+        struct functionDescription
+        {
+            public bool Windowed;
+            public int SystemRequirement;
+            public Dictionary<string, int> Requirement;
+            public string GroupName;
+            public string UtilName;
+            public string ClassName;
+            public int Version;
+            public int buttonX;
+            public int buttonY;
+            public FileInfo ffi;
+            public Assembly ass;
+        }
+        Dictionary<string, functionDescription> functionMap;
         public Form1()
         {
             InitializeComponent();
+            functionMap = new Dictionary<string, functionDescription>();
+        }
+        public int refreshFunction()
+        {
+            DirectoryInfo di = new DirectoryInfo(Application.StartupPath);
+            FileInfo[] fi = di.GetFiles("*.dll");
+            int nextButtonLocationX = initButtonX+incrementX;
+            int nextButtonLocationY = initButtonY+incrementY;
+            int groupCount = 1;
+            int funcCount = 0;
+            foreach (FileInfo ffi in fi)
+            {
+                Assembly ass = Assembly.LoadFile(Application.StartupPath + "/" + ffi.Name);
+                Type tp = ass.GetType(ffi.Name.Substring(0, ffi.Name.IndexOf(@".dll")) + @".Description");
+                if (tp == null)
+                {
+                    MessageBox.Show("Illegal component.File:" + ffi.Name+@":Cannot desolve a component which don't have a Description class","Loading Failiure");
+                    continue;
+                }
+                functionDescription desStruct;
+                Object obj = Activator.CreateInstance(tp);
+                MethodInfo CN = tp.GetMethod("ClassName");
+                string className = CN.Invoke(obj, null).ToString();
+                //Check for replica class
+                bool flag = false;
+                foreach (KeyValuePair<string, functionDescription> kvp in functionMap)
+                {
+                    if (kvp.Value.ClassName.Equals(className))
+                    {
+                        MessageBox.Show("Invalid component.File:" + ffi.Name + " and file:"+kvp.Value.ffi.Name+":Duplicated class name:"+kvp.Key, "Loading Failiure");
+                        flag = true;
+                        continue;
+                    }
+                }
+                if (flag)
+                {
+                    continue;
+                }
+                desStruct.ClassName = className;
+
+                MethodInfo UN = tp.GetMethod("UtilName");
+                string utilName = UN.Invoke(obj, null).ToString();
+                desStruct.UtilName = utilName;
+
+                MethodInfo Rq = tp.GetMethod("Requirement");
+                Dictionary<string, int> requirement = (Dictionary<string, int>)Rq.Invoke(obj, null);
+                desStruct.Requirement = requirement;
+
+                MethodInfo SR = tp.GetMethod("SystemRequirement");
+                int systemRequirement = (int)SR.Invoke(obj, null);
+                desStruct.SystemRequirement = systemRequirement;
+                if (systemRequirement > Version)
+                {
+                    MessageBox.Show("Invalid component.File:" + ffi.Name + "don't support current framework version", "Loading Failiure");
+                    continue;
+                }
+
+                MethodInfo Wd = tp.GetMethod("Windowed");
+                bool windowed = (bool)Wd.Invoke(obj, null);
+                desStruct.Windowed = windowed;
+
+                MethodInfo GN = tp.GetMethod("GroupName");
+                string groupName = GN.Invoke(obj, null).ToString();
+                if (!groupName.Equals("Internal"))
+                {
+                    groupCount++;
+                }
+                desStruct.GroupName = groupName;
+
+                MethodInfo Vn = tp.GetMethod("Version");
+                int version = (int)Vn.Invoke(obj, null);
+                desStruct.Version = version;
+
+
+                if (!utilName.Equals("Internal"))
+                {
+                    Button newb = new Button();
+                    newb.Name = className;
+                    newb.Text = utilName;
+                    if (groupCount == 1)
+                    {
+
+                    }
+                    newb.Size = new Size(75, 23);
+                    newb.Location = new Point(nextButtonLocationX, nextButtonLocationY);
+                    nextButtonLocationX = 81 + nextButtonLocationX;
+                    newb.MouseClick += Newb_MouseClick;
+                    this.Controls.Add(newb);
+                }
+
+                desStruct.ffi = ffi;
+                desStruct.ass = ass;
+                desStruct.buttonX = nextButtonLocationX;
+                desStruct.buttonY = nextButtonLocationY;
+
+                functionMap.Add(className, desStruct);
+            }
+            if (requirementNotMet())
+            {
+                MessageBox.Show("Requirement not met.Several components will not be loaded", "Loading Failiure");
+            }
+            return funcCount;
+        }
+        private void Newb_MouseClick(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Assembly ass = functionMap[btn.Name].ass;
+            FileInfo ffi = functionMap[btn.Name].ffi;
+            if (functionMap[btn.Name].Windowed)
+            {
+                Type ts = ass.GetType(ffi.Name.Substring(0, ffi.Name.IndexOf(@".dll")) + @".Form1", false);
+                (Activator.CreateInstance(ts) as Form).Show();
+            }
+            else
+            {
+                Type tp = ass.GetType(ffi.Name.Substring(0, ffi.Name.IndexOf(@".dll")) + @"."+ functionMap[btn.Name].ClassName);
+                object obj = Activator.CreateInstance(tp);
+            }
+        }
+
+        
+        private bool requirementNotMet()
+        {
+            string unmetRequirement = "";
+            foreach (KeyValuePair<string, functionDescription> kvp in functionMap)
+            {
+                functionDescription desStruct = kvp.Value;
+                Dictionary<string, int> Requirement = desStruct.Requirement;
+                foreach(KeyValuePair<string,int> rqp in Requirement)
+                {
+                    string reqName = rqp.Key;
+                    int reqVern = rqp.Value;
+                    if (reqVern > functionMap[reqName].Version)
+                    {
+                        unmetRequirement+="Function "+kvp.Key+" requires function "+reqName+" on version "+
+                        
+                    }
+                }
+            }
+            if (unmetRequirement.Equals(""))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
